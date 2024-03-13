36e7543cd484e4e2484b022481afaf8ea9766cf1
yyysolhhh <solbaram37@gmail.com>
2024-03-13 17:56:39 +0900

    [Tutorial4] updating our serializer

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -14,9 +14,11 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
 # class SnippetSerializer(serializers.Serializer):
 #     id = serializers.IntegerField(read_only=True)
