672b6a94f488abd76f2ccf270524c15f3ef0f8f4
yyysolhhh <solbaram37@gmail.com>
2024-03-15 01:17:28 +0900

    [Tutorial6] using Routers

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,39 +1,51 @@
-from django.urls import path
-from rest_framework import renderers
-from rest_framework.urlpatterns import format_suffix_patterns
-# from snippets import views
-from snippets.views import api_root, SnippetViewSet, UserViewSet
+from django.urls import path, include
+# from rest_framework import renderers
+# from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from snippets import views
+# from snippets.views import api_root, SnippetViewSet, UserViewSet
 
 
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
+# Create a router and register our ViewSets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+# snippet_list = SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# snippet_detail = SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# snippet_highlight = SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
 
 # API endpoints
-urlpatterns = format_suffix_patterns([
-    path('', api_root),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail')
-])
+# urlpatterns = format_suffix_patterns([
+#     path('', api_root),
+#     path('snippets/', snippet_list, name='snippet-list'),
+#     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+#     path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+#     path('users/', user_list, name='user-list'),
+#     path('users/<int:pk>/', user_detail, name='user-detail')
+# ])
 
 # urlpatterns = format_suffix_patterns([
 #     path('', views.api_root),
